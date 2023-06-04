a2456c98495b20aa66612393389e4aa66891dc72
Fady <fady.michel@outlook.com>
2023-06-04 18:02:44 +0300

    react-tfjs model done

--- 05-Tensorflow-js-and-react/ai-moderator/src/App.js
+++ 05-Tensorflow-js-and-react/ai-moderator/src/App.js
@@ -3,15 +3,40 @@ import { UserText } from "./components/UserText";
 import { ModeratorNotifications } from "./components/ModeratorNotifications";
 
 import { useState } from "react";
+import { useEffect } from "react";
+
+import * as toxicityClassifier from "@tensorflow-models/toxicity";
 
 function App() {
-  // const [userText, setUserText] = useState("");
   const [textToxicity, setTextToxicity] = useState([]);
+  const [model, setModel] = useState(null);
 
-  const predictToxicity = (event) => {
-    // setUserText(event.target.value);
-    setTextToxicity(["toxic", "insult"]);
+  const predictToxicity = async (event) => {
+    const predictions = await model.classify([event.target.value]);
+    setTextToxicity(
+      // Sets the "textToxicity" array
+      // to the predictions after some filtering and mapping.
+      // (console.log) the predictions to see
+      // from where this came from.
+      predictions
+        .filter((item) => item.results[0].match === true)
+        .map((item) => item.label)
+    );
   };
+
+  useEffect(() => {
+    async function loadModel() {
+      // "threshold" The the confidence interval for the classier.
+      // Higher values = the model is more confident about its prediction.
+      const threshold = 0.6;
+      const toxicityModel = await toxicityClassifier.load(threshold);
+      setModel(toxicityModel);
+    }
+    if (model === null) {
+      // Only load the model if its current value is null
+      loadModel();
+    }
+  }, [textToxicity, model]); // Watch the values for those and execute when changed.
 
   return (
     <div>
